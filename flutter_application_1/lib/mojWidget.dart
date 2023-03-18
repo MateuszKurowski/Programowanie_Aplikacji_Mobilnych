ac4184a1b2368d2822c504a47c726ce22712c8a2
Mateusz Kurowski <mateusz.kurowski@microsoft.wsei.edu.pl>
2023-03-18 14:44:27 +0100

    Lab1

---
+++ flutter_application_1/lib/mojWidget.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class MojWidget extends StatelessWidget {
+  const MojWidget({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Text("To jest moj widget.");
+  }
+}
